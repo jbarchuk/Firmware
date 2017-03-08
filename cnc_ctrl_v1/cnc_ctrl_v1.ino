eaee9a07cd89d6618ce90b6eba3c862dbcd863ed
Barbour Smith <bar@makesmithtech.com>
2017-03-08 11:35:26 -0800

    remove forward kinematics

--- cnc_ctrl_v1/cnc_ctrl_v1.ino
+++ cnc_ctrl_v1/cnc_ctrl_v1.ino
@@ -32,7 +32,7 @@ void setup(){
     Timer1.initialize(10000);
     Timer1.attachInterrupt(runsOnATimer);
     
-    kinematics.forward(leftAxis.setpoint(), rightAxis.setpoint(), &xTarget, &yTarget); //setup the targets to be correct
+    //kinematics.forward(leftAxis.setpoint(), rightAxis.setpoint(), &xTarget, &yTarget); //setup the targets to be correct
     
 }
 
